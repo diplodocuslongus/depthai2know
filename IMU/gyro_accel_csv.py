593825294213455949b7e06b7b8c18e2365ebe07
diplodocuslongus <hinoki.kuaimu@gmail.com>
2024-06-04 10:18:40 +0800

    naming convention, auto check path, create if not

--- IMU/gyro_accel_csv.py
+++ IMU/gyro_accel_csv.py
@@ -10,8 +10,11 @@
 # gyrX,...: component of raw gyroscope, rad/s
 # https://docs.luxonis.com/projects/api/en/latest/components/nodes/imu/
 # added system log TODO finish
+# added: drone name (option), just set time as a string with any time unit (s,mn,hr), 
+# check for directory, is not exisiting create it# adjust file name convention 
 
 import os # for file manipulation
+from pathlib import Path
 import depthai as dai
 import time
 import math
@@ -32,7 +35,29 @@ def logChipTemperature(info):
     t = info.chipTemperature
     print(f"Chip temperature - average: {t.average:.2f}, css: {t.css:.2f}, mss: {t.mss:.2f}, upa: {t.upa:.2f}, dss: {t.dss:.2f}")
 
+#-----------------------------------
 # parameters for the data collection
+#-----------------------------------
+# hardware / system setup information
+hw_setup_idx = 0 # input idx corresponding to the current configuration below
+HW_SETUP= {0:'Drone',1:'Cart',2:'Static',3:'BenchMotorOn',4:'Other',5:'',6:'',7:'',8:''}
+# Drone / frame name (if applicable)
+drone_frame_idx = 1
+DRONE_FRAME= {0:'380mmFrame',1:'WarehouseFrame',2:'225mmFrame',3:'',4:'',5:'',6:'',7:'',8:''}
+FLIGHT_MODE = 'Manual'
+#FLIGHT_MODE = 'Loiter'
+# imu orientation idx
+# put in a dict for conveniently setting the output csv filename
+# imuTK: for use with the imu Tool Kit (imu_tk)
+# 'level' means the imu (or the frame in which the chip is mounted, e.g. the oak camera) is perfectly level; different from xDown where x points down but may not be perfectly level
+imu_orient = 4
+IMU_ORIENTATION = {0:'level',1:'xDown',2:'xUp',3:'xDown',4:'yUp',5:'yDown',6:'zUp',7:'zDown',8:'imuTK'}
+
+if HW_SETUP[hw_setup_idx] == 'Drone':
+    CONFIG_DESCRIPTION = f'{DRONE_FRAME[drone_frame_idx]}_{FLIGHT_MODE}Mode'
+else:    
+    CONFIG_DESCRIPTION = f'{HW_SETUP[hw_setup_idx]}_{IMU_ORIENTATION[imu_orient]}'
+
 #OAK_NAME = 'OAKDPro'
 OAK_NAME = 'OAKLight'
 if OAK_NAME == 'OAKDPro':
@@ -42,36 +67,57 @@ if OAK_NAME == 'OAKDPro':
     #IMU_GYRO_SR = 100 # frequency / sample rate of the gyro, in Hz
     IMU_NAME = 'BNO086'
 elif OAK_NAME == 'OAKLight':
-    IMU_ACCEL_SR = 250 
-    IMU_GYRO_SR = 250
+    IMU_ACCEL_SR = 200 
+    IMU_GYRO_SR = 200
     IMU_NAME = 'BMI270'
 
-ACQ_T_HR = 1.5 #0.1 #0.1 #1.5
+# total acquisition time
+# new: simply write the time as a string, with units of s (seconds), mn (minute) or hr
+# the proper total time in seconds will be automatically extracted from the string
+
+TIME_STR = '2mn'
+if 'hr' in TIME_STR:
+    acq_time_hr = int(TIME_STR.split('hr')[0])
+    ACQ_T = acq_time_hr*3600
+    print(f'acquisition time in hour, that is {ACQ_T}s')
+elif 'mn' in TIME_STR:
+    acq_time_mn = int(TIME_STR.split('mn')[0])
+    ACQ_T = acq_time_mn*60
+    print(f'acquisition time in minute, that is {ACQ_T}s')
+elif 's' in TIME_STR:
+    acq_time_s = int(TIME_STR.split('s')[0])
+    ACQ_T = acq_time_s
+    print(f'acquisition time in second, that is {ACQ_T}s')
+
+#ACQ_T_HR = 1.5 #0.1 #0.1 #1.5
 #ACQ_T = ACQ_T_HR * 3600 # total acquisition time in seconds
-ACQ_T = 10 # total acquisition time in seconds
+#ACQ_T = 10 # total acquisition time in seconds
 HOME = os.getenv("HOME")
 N_SAMPLES = int(ACQ_T) * IMU_GYRO_SR
 #N_SAMPLES = 10 #int(ACQ_T) * IMU_GYRO_SR
 SHOW_DATA = False # show live data in terminal
 LOG_CPU_TEMPERATURE = False
-# put the imu orientation in a dict for conveniently setting the output csv filename
-# imuTK: for use with the imu Tool Kit (imu_tk)
-# 'level' means the imu (or the frame in which the chip is mounted, e.g. the oak camera) is perfectly level; different from xDown where x points down but may not be perfectly level
-IMU_ORIENTATION = {0:'level',1:'xDown',2:'xUp',3:'xDown',4:'yUp',5:'yDown',6:'zUp',7:'zDown',8:'imuTK'}
-imu_orient = 4
-if ACQ_T_HR > 1.0:
-    TIME_STR = f'{ACQ_T_HR}hr'
-else:
-    TIME_STR = f'{int(ACQ_T_HR*60)}mn'
+#if ACQ_T_HR > 1.0:
+#    TIME_STR = f'{ACQ_T_HR}hr'
+#else:
+#    TIME_STR = f'{int(ACQ_T_HR*60)}mn'
 #CSV_FILENAME = 'check'
-CSV_FILENAME = f'{HOME}/Data/Drones/IMU/{OAK_NAME}_{IMU_NAME}_{TIME_STR}_gyroSR{IMU_GYRO_SR}_accSR{IMU_ACCEL_SR}_{IMU_ORIENTATION[imu_orient]}.csv'
+# check if directory exists if not , create it.
+if HW_SETUP[hw_setup_idx] == 'Drone':
+    SUB_DIR = DRONE_FRAME[drone_frame_idx]
+else:    
+    SUB_DIR = HW_SETUP[hw_setup_idx]
+DATA_PATH = f'{HOME}/Data/Drones/IMU/{SUB_DIR}'
+Path(DATA_PATH).mkdir(parents=True, exist_ok=True)
+# built the full filename
+if IMU_GYRO_SR == IMU_ACCEL_SR:
+    CSV_FILENAME = f'{DATA_PATH}/{OAK_NAME}_{IMU_NAME}_{TIME_STR}_SR{IMU_GYRO_SR}_{CONFIG_DESCRIPTION}.csv'
+else:
+    CSV_FILENAME = f'{DATA_PATH}/{OAK_NAME}_{IMU_NAME}_{TIME_STR}_gyroSR{IMU_GYRO_SR}_accSR{IMU_ACCEL_SR}_{CONFIG_DESCRIPTION}.csv'
 #CSV_FILENAME = f'{HOME}/Data/Drones/IMU/{OAK_NAME}_{IMU_NAME}_{TIME_STR}_gyroSR{IMU_GYRO_SR}_accSR{IMU_ACCEL_SR}_{IMU_ORIENTATION[imu_orient]}.csv'
-#CSV_FILENAME = f'oak_BNO086_{TIME_STR}_gyroSR{IMU_GYRO_SR}_accSR{IMU_ACCEL_SR}_{IMU_ORIENTATION[imu_orient]}.csv'
-#CSV_FILENAME = f'oak_BNO086_{TIME_STR}_gyroSR{IMU_GYRO_SR}_accSR{IMU_ACCEL_SR}_{IMU_ORIENTATION[3]}.csv'
-#CSV_FILENAME = f'oak_BNO086_{int(ACQ_T_HR*60)}mn_gyroSR{IMU_GYRO_SR}_accSR{IMU_ACCEL_SR}_{IMU_ORIENTATION[3]}.csv'
-#CSV_FILENAME = '/home/ludofw/Data/Drones/IMU/imu_oak_BNO086_2hr_02042024.csv'
-#CSV_FILENAME = '~/Data/Drones/IMU/imu_oak_BNO086_2hr_02042024.csv'
 
+print(f'saving csv to {CSV_FILENAME}')
+pouet
 # Create pipeline
 pipeline = dai.Pipeline()
 
